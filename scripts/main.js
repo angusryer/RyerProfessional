9af9988728fc9b61132b58c6d8df823bbbb55aac
aquinashub <46163463+angusryer@users.noreply.github.com>
2020-05-19 23:04:53 -0400

    LAst daily commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+function getInput() {
+    var userInput = document.getElementById("universalSearchInput");
+    document.getElementsByClassName("universal-search__return").innerHTML = "<p>"&userInput&"</p>";
+}
